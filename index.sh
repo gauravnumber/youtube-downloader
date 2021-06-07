4ae2510173b94f49f722288d4a86064751243c69
Gaurav <gauravnumber@gmail.com>
2021-06-07 19:11:37 +0530

    read command works

--- index.sh
+++ index.sh
@@ -1,3 +1,5 @@
-# echo "Enter your url"
-# read url
-wget -qO- https://www.youtube.com/watch\?v\=FBWr1KtnRcI | sed 's/http/\n\nhttp/g' | grep googlevideo | sed 's/"/\n/g' | grep googlevideo | grep videoplayback | sed 's/\\u0026/\&/g' | grep itag 
+echo "Enter your url"
+read url
+wget -qO- "$url" | sed 's/http/\n\nhttp/g' | grep googlevideo | sed 's/"/\n/g' | grep googlevideo | grep videoplayback | sed 's/\\u0026/\&/g' | grep itag 
+
+# https://www.youtube.com/watch\?v\=FBWr1KtnRcI
